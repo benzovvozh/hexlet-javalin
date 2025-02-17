7a3bfe243b10ab3cff20322843089a81527d20d5
benzovvozh <augusthoffman3@gmail.com>
2025-02-17 16:03:33 +0500

    created a website layout and templates

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,15 +1,22 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
 
 public class HelloWorld {
     public static void main(String[] args) {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
         });
-        app.get("/", ctx -> ctx.result("Hello World!"));
-        app.get("/users", ctx -> ctx.result("GET /users"));
-        app.post("/users", ctx -> ctx.result("POST /users"));
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+        app.get("/users", ctx -> {
+            ctx.render("users.jte");
+        });
+
         app.get("/hello", ctx -> {
             var name = ctx.queryParamAsClass("name", String.class).getOrDefault("Unknown");
             ctx.result("Hello, " + name + "!");
@@ -18,6 +25,10 @@ public class HelloWorld {
             var userId = ctx.pathParam("id");
             var postId = ctx.pathParam("postid");
             ctx.result("user id = " + userId + ", postid = " + postId);
+        });
+
+        app.get("/contacts", ctx -> {
+            ctx.render("contacts.jte");
         });
         app.start(7070);
     }
